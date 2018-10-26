3b21c9f327f94e10b87b5d3c85d99452cbd69db5
bjelkenhed <bjelkenhed@gmail.com>
2018-10-26 15:26:18 +0200

    ActiveMQ support

--- src/main/java/com/example/cloudstreamquickstart/CloudStreamQuickstartApplication.java
+++ src/main/java/com/example/cloudstreamquickstart/CloudStreamQuickstartApplication.java
@@ -2,11 +2,13 @@ package com.example.cloudstreamquickstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 
 @SpringBootApplication
 public class CloudStreamQuickstartApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudStreamQuickstartApplication.class, args);
+
 	}
 }
